9e3e6dda2a008cb95a1c2746713a1b653ff9cbe2
15rtrujillo <ryantrujillo@comcast.net>
2024-09-18 19:07:39 -0600

    Pretty sure it's good now.

--- Board.cs
+++ Board.cs
@@ -10,9 +10,9 @@ public class Board
 	public int Rows { get; private set; }
 	public int Columns { get; private set; }
 
-	private static readonly Dictionary<int, int> actualRowLookup = new();
-	private static readonly Dictionary<int, int> actualColumnLookup = new();
-	private static readonly Dictionary<int, long> columnBitmaskLookup = new();
+	private readonly Dictionary<int, int> _actualRowLookup = new();
+	private readonly Dictionary<int, int> _actualColumnLookup = new();
+	private readonly Dictionary<int, long> _columnBitmaskLookup = new();
 
 	private readonly long[,] _board;
 	private readonly int _actualColumns;
@@ -29,15 +29,15 @@ public class Board
 	{
 		get
 		{
-			int actualColumn = LookupActualColumn(column, _actualColumns);
+			int actualColumn = LookupActualColumn(column);
 			long bitmask = LookupBitmask(column);
-			return (_board[LookupActualRow(row, Rows), actualColumn] & bitmask) != 0;
+			return (_board[LookupActualRow(row), actualColumn] & bitmask) != 0;
 		}
 
 		set
 		{
-			int actualRow = LookupActualRow(row, Rows);
-			int actualColumn = LookupActualColumn(column, _actualColumns);
+			int actualRow = LookupActualRow(row);
+			int actualColumn = LookupActualColumn(column);
 			long bitmask = LookupBitmask(column);
 			if (value)
 			{
@@ -62,39 +62,85 @@ public class Board
 		}
 	}
 
-	private static int Mod(int a, int b)
+	private int LookupActualRow(int row)
 	{
-		return a - b * (int)Math.Floor((double)a / b);
-	}
-
-	private static int LookupActualRow(int row, int rows)
-	{
-		if (!actualRowLookup.TryGetValue(row, out int actualRow))
+		if (!_actualRowLookup.TryGetValue(row, out int actualRow))
 		{
-			actualRow = Mod(row, rows);
-			actualRowLookup[row] = actualRow;
+			if (row < 0)
+			{
+				if (row < -Rows)
+				{
+					actualRow = Rows + row / Rows;
+				}
+
+				else
+				{
+					actualRow = Rows + row;
+				}
+			}
+
+			else if (row > 0)
+			{
+				actualRow = row % Rows;
+			}
+
+			else
+			{
+				actualRow = 0;
+			}
+
+			_actualRowLookup[row] = actualRow;
 		}
 
 		return actualRow;
 	}
 
-	private static int LookupActualColumn(int column, int columns)
+	private int LookupActualColumn(int column)
 	{
-		if (!actualColumnLookup.TryGetValue(column, out int actualColumn))
+		if (!_actualColumnLookup.TryGetValue(column, out int actualColumn))
 		{
-			actualColumn = Mod((int)Math.Floor(column / 64.0), columns);
-			actualColumnLookup[column] = actualColumn;
+			if (column < 0)
+			{
+				actualColumn = _actualColumns + (int)Math.Floor(column / 64.0);
+			}
+
+			else if (column > 0)
+			{
+				actualColumn = (int)Math.Floor(column / 64.0) % _actualColumns;
+			}
+			
+			else
+			{
+				actualColumn = 0;
+			}
+
+			_actualColumnLookup[column] = actualColumn;
 		}
 
 		return actualColumn;
 	}
 
-	private static long LookupBitmask(int column)
+	private long LookupBitmask(int column)
 	{
-		if (!columnBitmaskLookup.TryGetValue(column, out long bitmask))
+		if (!_columnBitmaskLookup.TryGetValue(column, out long bitmask))
 		{
-			bitmask = 1 << (63 - Mod(column, 64));
-			columnBitmaskLookup[column] = bitmask;
+			if (column < 0)
+			{
+				// This will break on negative numbers that are too high, but I don't care right now.
+				bitmask = 1L << (-column - 1);
+			}
+
+			else if (column > 0)
+			{
+				bitmask = 1L << (63 - column % 64);
+			}
+
+			else
+			{
+				bitmask = 1L << 63;
+			}
+
+			_columnBitmaskLookup[column] = bitmask;
 		}
 
 		return bitmask;
