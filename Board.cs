5eea41a55b8037f87a9bc86a60c33eeabd9f5600
Rrrrry123 <ryantrujillo@comcast.net>
2024-09-26 12:56:56 -0600

    WIP improvements

--- Board.cs
+++ Board.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace GameOfLife;
 
@@ -10,43 +9,39 @@ public class Board
 	public int Rows { get; private set; }
 	public int Columns { get; private set; }
 
-	private readonly Dictionary<int, int> _actualRowLookup = new();
-	private readonly Dictionary<int, int> _actualColumnLookup = new();
-	private readonly Dictionary<int, long> _columnBitmaskLookup = new();
-
-	private readonly long[,] _board;
+	private readonly byte[] _board;
 	private readonly int _actualColumns;
 
 	public Board(int rows, int columns)
 	{
 		Rows = rows;
 		Columns = columns;
-		_actualColumns = columns / 64 + 1;
-		_board = new long[rows, _actualColumns];
+        _actualColumns = (int)Math.Ceiling((double)Columns / sizeof(byte));
+		_board = new byte[Rows * _actualColumns];
 	}
 
 	public bool this[int row, int column]
 	{
 		get
 		{
-			int actualColumn = LookupActualColumn(column);
-			long bitmask = LookupBitmask(column);
-			return (_board[LookupActualRow(row), actualColumn] & bitmask) != 0;
+            int location = BoundDimension(column / 8, _actualColumns) + (_actualColumns * BoundDimension(row, Rows));
+			byte bitmask = CalculateBitmask(column, Columns);
+			return (_board[location] & bitmask) != 0;
 		}
 
 		set
 		{
-			int actualRow = LookupActualRow(row);
-			int actualColumn = LookupActualColumn(column);
-			long bitmask = LookupBitmask(column);
+			int location = BoundDimension(column / 8, _actualColumns) + (_actualColumns * BoundDimension(row, Rows));
+			byte bitmask = CalculateBitmask(column, Columns);
+            byte currentValue = _board[location];
 			if (value)
 			{
-				_board[actualRow, actualColumn] = _board[actualRow, actualColumn] | bitmask;
+				_board[location] = (byte)(currentValue | bitmask);
 			}
 
 			else
 			{
-				_board[actualRow, actualColumn] = _board[actualRow, actualColumn] & ~bitmask;
+				_board[location] = (byte)(currentValue & ~bitmask);
 			}
 		}
 	}
@@ -62,87 +57,33 @@ public class Board
 		}
 	}
 
-	private int LookupActualRow(int row)
-	{
-		if (!_actualRowLookup.TryGetValue(row, out int actualRow))
-		{
-			if (row < 0)
-			{
-				if (row < -Rows)
-				{
-					actualRow = Rows + row / Rows;
-				}
+    private static int BoundDimension(int dimension, int max)
+    {
+        if (dimension >= max)
+        {
+            return 0;
+        }
 
-				else
-				{
-					actualRow = Rows + row;
-				}
-			}
+        if (dimension < 0)
+        {
+            return max - 1;
+        }
 
-			else if (row > 0)
-			{
-				actualRow = row % Rows;
-			}
+        return dimension;
+    }
 
-			else
-			{
-				actualRow = 0;
-			}
+    private static byte CalculateBitmask(int position, int max)
+    {
+        if (position < 0)
+        {
+            position = max - 1;
+        }
 
-			_actualRowLookup[row] = actualRow;
-		}
+        else if (position >= max)
+        {
+            position = 0;
+        }
 
-		return actualRow;
-	}
-
-	private int LookupActualColumn(int column)
-	{
-		if (!_actualColumnLookup.TryGetValue(column, out int actualColumn))
-		{
-			if (column < 0)
-			{
-				actualColumn = _actualColumns + (int)Math.Floor(column / 64.0);
-			}
-
-			else if (column > 0)
-			{
-				actualColumn = (int)Math.Floor(column / 64.0) % _actualColumns;
-			}
-			
-			else
-			{
-				actualColumn = 0;
-			}
-
-			_actualColumnLookup[column] = actualColumn;
-		}
-
-		return actualColumn;
-	}
-
-	private long LookupBitmask(int column)
-	{
-		if (!_columnBitmaskLookup.TryGetValue(column, out long bitmask))
-		{
-			if (column < 0)
-			{
-				// This will break on negative numbers that are too high, but I don't care right now.
-				bitmask = 1L << (-column - 1);
-			}
-
-			else if (column > 0)
-			{
-				bitmask = 1L << (63 - column % 64);
-			}
-
-			else
-			{
-				bitmask = 1L << 63;
-			}
-
-			_columnBitmaskLookup[column] = bitmask;
-		}
-
-		return bitmask;
-	}
+        return (byte)(1 << (position % sizeof(byte)));
+    }
 }
